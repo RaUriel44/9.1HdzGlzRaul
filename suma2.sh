bdb255ff38fd36179448e828f507588cc117af2c
RaUriel44 <rhernandez10501@plataforma-utslp.net>
2025-01-18 12:22:08 -0600

    Update suma2.sh

--- suma2.sh
+++ suma2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -n "Ingresa el primer número: "
+echo -n "ingresa el primer numero: "
 read num1
 
-echo -n "Ingresa el segundo número: "
+echo -n "ingresa el segundo número: "
 read num2
 
 suma=$((num1 + num2))
-resta=$((num1 - num2))  # Cambié para restar correctamente num1 y num2
+resta=$((num1 - num2))
 
 echo "La suma de $num1 y $num2 es: $suma"
-echo "La resta de $num1 menos $num2 es: $resta"
+echo "La resta de $suma menos $num3 es: $resta"
