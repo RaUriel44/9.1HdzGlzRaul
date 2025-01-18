df727ce563205c2395d5ddb7f4d8662957f539f6
RaUriel44 <rhernandez10501@plataforma-utslp.net>
2025-01-18 12:44:59 -0600

    Update suma2.sh

---
+++ suma2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -n "ingresa el primer numero: "
+read num1
+
+echo -n "ingresa el segundo número: "
+read num2
+
+suma=$((num1 + num2))
+resta=$((num1 - num2))
+
+echo "La suma de $num1 y $num2 es: $suma"
+echo "La resta de $suma menos $num3 es: $resta"
